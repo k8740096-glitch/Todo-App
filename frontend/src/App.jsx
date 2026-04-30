7f6ac4242a8fdc28a18e8c95b7b173b6dca77710
Khushboo <k8740096gmail.com>
2026-04-30 11:44:26 +0530

    Initial commit - Todo App Ready

---
+++ frontend/src/App.jsx
@@ -0,0 +1,67 @@
+import {useEffect, useState} from "react";
+import "./App.css";
+function App() {
+  const [todos, setTodos] = useState([]);
+  const [text, setText] = useState("");
+  const fetchTodos = async () => {
+    const res = await fetch("http://localhost:5000/api/todos");
+    const data = await res.json();
+    setTodos(data);
+  };
+  useEffect(() => {
+    fetchTodos();
+  },[]);
+  const addTodo = async () => {
+    if(!text) return;
+    await fetch("http://localhost:5000/api/todos",{
+      method: "POST",
+      headers: {"Content-Type":"application/json"},
+      body: JSON.stringify({text}),
+    });
+    setText("");
+    fetchTodos();
+  };
+  const toggleTodo = async (id, completed) => {
+    await fetch(`http://localhost:5000/api/todos/${id}`,{
+      method: "PUT",
+      headers: {"Content-Type":"application/json"},
+      body: JSON.stringify({completed: !completed}),
+    });
+    fetchTodos();
+  };
+  const deleteTodo = async (id) => {
+    await fetch(`http://localhost:5000/api/todos/${id}`,{
+      method: "DELETE",
+    });
+    fetchTodos();
+  };
+  return(
+    <div className="container">
+      <div className="todo-box">
+      <h2>To-Do List</h2>
+      <div className="input-section">
+      <input 
+      value={text}
+      onChange={(e) => setText(e.target.value)}
+      />
+      <button onClick={addTodo}>Add</button>
+      </div>
+      <ul>
+        {todos.map((todo)=> (
+          <li key={todo._id}>
+            <span 
+            onClick={() => toggleTodo(todo._id, todo.completed)}
+            
+            >
+            {todo.text}
+            </span>
+            <button onClick={()=> deleteTodo(todo._id)}>❌</button>
+          </li>
+        ))}
+      </ul>
+      </div>
+    </div>
+    
+  );
+}
+export default App;
