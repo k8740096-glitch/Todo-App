9efb9331aa1d624eff302a6df1e34d31bb2ade9c
Khushboo <k8740096gmail.com>
2026-05-01 12:05:42 +0530

    fix api url

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,7 +4,7 @@ function App() {
   const [todos, setTodos] = useState([]);
   const [text, setText] = useState("");
   const fetchTodos = async () => {
-    const res = await fetch("http://localhost:5000/api/todos");
+    const res = await fetch("https://todo-app-lnf1.onrender.com/api/todos");
     const data = await res.json();
     setTodos(data);
   };
@@ -13,7 +13,7 @@ function App() {
   },[]);
   const addTodo = async () => {
     if(!text) return;
-    await fetch("http://localhost:5000/api/todos",{
+    await fetch("https://todo-app-lnf1.onrender.com/api/todos",{
       method: "POST",
       headers: {"Content-Type":"application/json"},
       body: JSON.stringify({text}),
@@ -22,7 +22,7 @@ function App() {
     fetchTodos();
   };
   const toggleTodo = async (id, completed) => {
-    await fetch(`http://localhost:5000/api/todos/${id}`,{
+    await fetch(`https://todo-app-lnf1.onrender.com/api/todos/${id}`,{
       method: "PUT",
       headers: {"Content-Type":"application/json"},
       body: JSON.stringify({completed: !completed}),
@@ -30,7 +30,7 @@ function App() {
     fetchTodos();
   };
   const deleteTodo = async (id) => {
-    await fetch(`http://localhost:5000/api/todos/${id}`,{
+    await fetch(`https://todo-app-lnf1.onrender.com/sapi/todos/${id}`,{
       method: "DELETE",
     });
     fetchTodos();
